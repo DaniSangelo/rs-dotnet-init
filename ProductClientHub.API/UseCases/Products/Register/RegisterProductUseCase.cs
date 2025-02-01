14bdb0f8734a0cc4883b2e6764567e9b10d28de5
Daniel Sângelo <danielsangelo@hotmail.com>
2025-02-01 13:02:40 -0300

    Create product

---
+++ ProductClientHub.API/UseCases/Products/Register/RegisterProductUseCase.cs
@@ -0,0 +1,50 @@
+﻿using ProductClientHub.API.Entities;
+using ProductClientHub.API.Infra;
+using ProductClientHub.API.UseCases.Products.SharedValidator;
+using ProductClientHub.Communication.Requests;
+using ProductClientHub.Communication.Responses;
+using ProductClientHub.Exceptions.ExceptionsBase;
+
+namespace ProductClientHub.API.UseCases.Products.Register
+{
+    public class RegisterProductUseCase
+    {
+        public ResponseShortProductJson Execute(Guid clientId, RequestProductJson request)
+        {
+            var dbContext = new ProductClientHubDbContext();
+            Validate(dbContext, clientId, request);
+
+            var product = new Product
+            {
+                Name = request.Name,
+                Brand = request.Brand,
+                Price = request.Price,
+                ClientId = clientId,
+            };
+
+            dbContext.Products.Add(product);
+            dbContext.SaveChanges();
+
+            return new ResponseShortProductJson
+            {
+                Id = product.Id,
+                Name = product.Name
+            };
+        }
+
+        public static void Validate(ProductClientHubDbContext dbContext, Guid clientId, RequestProductJson request)
+        {
+            var clientExist = dbContext.Clients.Any(c => c.Id == clientId);
+            if (!clientExist) throw new NotFoundException("Client does not exist");
+            var validator = new RequestProductValidator();
+            var result = validator.Validate(request);
+
+            if (!result.IsValid)
+            {
+                var errors = result.Errors.Select(f => f.ErrorMessage).ToList();
+
+                throw new ErrorOnValidationException(errors);
+            }
+        }
+    }
+}
---
+++ ProductClientHub.API/UseCases/Products/SharedValidator/RequestProductValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using ProductClientHub.Communication.Requests;
+
+namespace ProductClientHub.API.UseCases.Products.SharedValidator
+{
+    public class RequestProductValidator : AbstractValidator<RequestProductJson>
+    {
+        public RequestProductValidator()
+        {
+            RuleFor(product => product.Name).NotEmpty().WithMessage("Product name must be informed");
+            RuleFor(product => product.Brand).NotEmpty().WithMessage("Product brand must be informed");
+            RuleFor(product => product.Price).GreaterThan(0).WithMessage("Product price must be greater than zero");
+        }
+    }
+}
