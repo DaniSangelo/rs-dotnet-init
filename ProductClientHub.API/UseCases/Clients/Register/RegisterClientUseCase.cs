398e4aa369a83e924e974011afae799e731d9c60
Daniel Sângelo <danielsangelo@hotmail.com>
2025-02-01 10:40:37 -0300

    Initial commit

---
+++ ProductClientHub.API/UseCases/Clients/Register/RegisterClientUseCase.cs
@@ -0,0 +1,22 @@
+﻿using ProducClientHub.Exceptions.ExceptionsBase;
+using ProductClientHub.Communication.Requests;
+using ProductClientHub.Communication.Responses;
+
+namespace ProductClientHub.API.UseCases.Clients.Register
+{
+    public class RegisterClientUseCase
+    {
+        public ResponseClientJson Execute(RequestClientJson request)
+        {
+            var validator = new RegisterClientValidator();
+            var result = validator.Validate(request);
+            if (!result.IsValid)
+            {
+                var errors = result.Errors.Select(failure => failure.ErrorMessage).ToList();
+
+                throw new ErrorOnValidationException(errors);
+            }
+            return new ResponseClientJson();
+        }
+    }
+}
---
+++ ProductClientHub.Communication/Responses/ResponseErrorMessagesJson.cs
@@ -0,0 +1,17 @@
+﻿namespace ProductClientHub.Communication.Responses
+{
+    public class ResponseErrorMessagesJson
+    {
+        public List<string> Errors { get; private set; }
+
+        public ResponseErrorMessagesJson(string message)
+        {
+            Errors = [message]; //Errors = new List<string> { message };
+        }
+        
+        public ResponseErrorMessagesJson(List<string> messages)
+        {
+            Errors = messages;
+        }
+    }
+}
