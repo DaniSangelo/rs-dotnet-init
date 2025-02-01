9052cd2b57ec11f1d79a766c45dbeac3fcc09a8e
Daniel Sângelo <danielsangelo@hotmail.com>
2025-02-01 13:48:52 -0300

    Get clients by id

---
+++ ProductClientHub.API/UseCases/Clients/GetById/GetClientByIdUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductClientHub.API.Infra;
+using ProductClientHub.Communication.Responses;
+using ProductClientHub.Exceptions.ExceptionsBase;
+
+namespace ProductClientHub.API.UseCases.Clients.GetById
+{
+    public class GetClientByIdUseCase
+    {
+        public ResponseClientJson Execute(Guid id)
+        {
+            var dbContext = new ProductClientHubDbContext();
+            var client = dbContext
+                .Clients
+                .Include(client => client.Products)
+                .FirstOrDefault(c => c.Id == id) ?? throw new NotFoundException("Client not found");
+            return new ResponseClientJson
+            {
+                Id = client.Id,
+                Name = client.Name,
+                Email = client.Email,
+                Products = client.Products.Select(p => new ResponseShortProductJson
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                }).ToList()
+            };
+        }
+    }
+}
