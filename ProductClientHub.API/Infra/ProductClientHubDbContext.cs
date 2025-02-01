f739eb3ac43a12e3ffffca2dacdc73c6522bf32d
Daniel Sângelo <danielsangelo@hotmail.com>
2025-02-01 11:14:26 -0300

    Create entities and set up entity framework

---
+++ ProductClientHub.API/Infra/ProductClientHubDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductClientHub.API.Entities;
+
+namespace ProductClientHub.API.Infra
+{
+    public class ProductClientHubDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\Lenovo\\Documents\\Estudos\\TI\\Rocketseat\\c-sharp\\ProductClientHubDB.db");
+        }
+    }
+}
